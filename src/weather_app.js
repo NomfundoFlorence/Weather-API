491730f56330e2369f16e694444c16d76079c24d
Nomfundo Florence <nomfundomlangeni0@gmail.com>
2025-03-19 17:45:38 +0200

    changed format of returned coordinates

--- src/weather_app.js
+++ src/weather_app.js
@@ -1,26 +1,30 @@
 const axios = require("axios");
 require("dotenv").config();
 
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
 const getCoordinates = async (location) => {
   const response = await axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${process.env.WEATHER_API_KEY}`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${WEATHER_API_KEY}`
   );
 
-  const latitude = response.data[0].lat;
-  const longitude = response.data[0].lon;
+  const coordinates = {
+    latitude: response.data[0].lat,
+    longitude: response.data[0].lon,
+  };
 
-  return [latitude, longitude];
+  return coordinates;
 };
 
 const getWeather = async (location) => {
-  const [latitude, longitude] = await getCoordinates(location);
+  const { latitude, longitude } = await getCoordinates(location);
 
   const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.WEATHER_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
   );
 
   return response.data;
 };
 
-getCoordinates("Duduza").then((data) => console.log(data));
-getWeather("Duduza").then((data) => console.log(data));
+// getCoordinates("Johannesburg").then((data) => console.log(data));
+// getWeather("Johannesburg").then((data) => console.log(data));
